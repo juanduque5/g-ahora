8224687218f204ef9de535337691afe29c44dd7f
juanduque5 <juanduque525@gmail.com>
2023-11-28 14:37:46 -0800

    newChange

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline text-green-400">
-        Hello world!
+        Hello worlddd!
       </h1>
     </div>
   );
