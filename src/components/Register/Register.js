2203be7b2d987c373a54f2be2aebbe489d93f204
juanduque5 <juanduque525@gmail.com>
2023-12-06 16:43:09 -0800

    Login Updates

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,6 +3,7 @@ import "./Register.css";
 
 import visible from "../../images/visible.png";
 import { Link, useNavigate } from "react-router-dom";
+import Modal from "react-modal";
 
 const Login = () => {
   const [name, setName] = useState("");
@@ -10,19 +11,23 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
   const [error, setError] = useState(null);
+  const [isModalOpen, setIsModalOPen] = useState(false);
+  const [message, setMessage] = useState([]);
 
   const [showPassword, setShowPassword] = useState(false);
   const [showPassword2, setShowPassword2] = useState(false);
   const navigate = useNavigate();
+
   //Handle Sign up API
 
   const handleSignupClick = () => {
-    if (password !== password2) {
-      setError(true);
-      return;
-    } else {
-      setError(false);
-    }
+    // if (password !== password2) {
+    //   setIsModalOPen(true);
+
+    //   return;
+    // } else {
+    //   setError(false);
+    // }
 
     const postData = {
       email: email,
@@ -41,17 +46,36 @@ const Login = () => {
     fetch("http://localhost:2001/auth/signup", requestOptions)
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          // En el caso de un error, maneja el JSON que envía el backend
+          return response.json().then((errorData) => {
+            if (password !== password2) {
+              const newErrorMessages = errorData.data.map((error) => error.msg);
+              setMessage([...newErrorMessages, "Passwords does not match"]);
+              throw new Error(`Error: ${errorData.message}`);
+            } else {
+              const newErrorMessages = errorData.data.map((error) => error.msg);
+              setMessage([...newErrorMessages]);
+              throw new Error(`Error: ${errorData.message}`);
+            }
+          });
+        } else if (password !== password2) {
+          setMessage(["Passwords does not match"]);
+          setIsModalOPen(true);
+          setError(false);
+          throw new Error(`Error: passwords`);
         }
         return response.json();
       })
       .then((data) => {
-        console.log(data);
+        console.log("data", data);
+
         navigate("/Login"); // Reemplaza '/dashboard' con la ruta deseada
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error("Error :", error);
+
         setError("Error al registrar. Por favor, inténtalo de nuevo.");
+        openModal();
       });
   };
 
@@ -80,11 +104,22 @@ const Login = () => {
     setShowPassword2(!showPassword2);
   };
 
+  //
+
+  const openModal = () => {
+    setIsModalOPen(true);
+  };
+  const closeModal = () => {
+    setIsModalOPen(false);
+  };
+
+  console.log("M", message);
+
   //Creating Sign up form
   return (
     <div className="wx">
       <div id="img" className="  flex w-1/2 justify-center">
-        <div className="xl:w-30 h-90 relative m-auto w-10/12  rounded-2xl bg-white shadow-2xl sm:h-5/6 sm:w-3/5 md:left-52 md:h-5/6 md:w-2/5 lg:h-5/6 xl:left-96 xl:h-5/6">
+        <div className="relative m-auto h-90 w-10/12 rounded-2xl  bg-white shadow-2xl sm:h-5/6 sm:w-3/5 md:left-52 md:h-5/6 md:w-2/5 lg:h-5/6 xl:left-96 xl:h-5/6 xl:w-30">
           <div className="h-13 ">
             <div className="flex h-full items-end justify-center ">
               <p className="font-open-sans text-2xl font-semibold">
@@ -168,29 +203,52 @@ const Login = () => {
           </div>
           <div className="relative top-2 m-auto flex h-5 w-11/12 ">
             <div className="flex h-1/2  cursor-pointer rounded-xl">
-              <p className="font-open-sans  cursor-pointer text-base text-black">
+              <p className="cursor-pointer  font-open-sans text-base text-black">
                 Already have an account ?{" "}
-                <span className="text-blue-new cursor-pointer">
+                <span className="cursor-pointer text-blue-new">
                   <Link to="/Login"> Inicia session </Link>
                 </span>
               </p>
             </div>
           </div>
-          <div className="h-15 mt-2 flex">
+          <div className="mt-2 flex h-15">
             <button
               onClick={handleSignupClick}
               disabled={error}
               style={{ opacity: error ? 0.5 : 1 }}
-              className="bg-blue-new m-auto flex h-1/2 w-1/2 cursor-pointer rounded-xl"
+              className="m-auto flex h-1/2 w-1/2 cursor-pointer rounded-xl bg-blue-new"
             >
-              <p className="font-fira-sans  m-auto cursor-pointer text-lg text-white">
+              <p className="m-auto  cursor-pointer font-fira-sans text-lg text-white">
                 Register
               </p>
-              {error && <p className="text-red-500">{error}</p>}
             </button>
           </div>
         </div>
       </div>
+      <Modal isOpen={isModalOpen} onAfterClose={closeModal}>
+        <div className="flex flex-col gap-1">
+          <div className="  border-b-slate-400">
+            <p className="font-semibold">An Error Occurred</p>
+          </div>
+          {message && (
+            <div className="mt-2 h-28">
+              {message.map((err, index) => (
+                <ul key={index}>
+                  <li className="ml-1" key={index}>
+                    - {err}
+                  </li>
+                </ul>
+              ))}
+            </div>
+          )}
+
+          <div className="relative top-5 flex justify-end ">
+            <button className="flex h-9 w-1/4 justify-center rounded-md bg-red-500">
+              <p className="m-auto font-open-sans text-white ">Exit</p>
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import Modal from "react-modal";
 
 const root = createRoot(document.getElementById("root")); // Create a root instance
+
+// Configurar el elemento de la aplicación
+Modal.setAppElement("#root"); // Asegúrate de ajustar el selector según el ID de tu elemento raíz
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
